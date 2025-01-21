8e7a0b641ec89fc68619dfe727ed18f406968ec3
Suneito <adria@babot.cat>
2025-01-22 00:04:58 +0100

    update js

--- client/js/meteomon.js
+++ client/js/meteomon.js
@@ -39,32 +39,30 @@ function openWebSocket(token) {
 
     socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        document.getElementById('solar.solar').textContent = `${data.solar.solar} W/m²`;
-        document.getElementById('solar.uvi').textContent = data.solar.uvi;
-
-        document.getElementById('wind.gust').textContent = `${data.wind.gust} km/h`;
-        document.getElementById('wind.direction').textContent = `${data.wind.direction} °`;
-        document.getElementById('wind.speed').textContent = `${data.wind.speed} km/h`;
-
-        document.getElementById('pressure.absolute').textContent = `${data.pressure.absolute} hPa`;
-        document.getElementById('pressure.relative').textContent = `${data.pressure.relative} hPa`;
-
-        document.getElementById('rain.hourly').textContent = `${data.rain.hourly} mm`;
-        document.getElementById('rain.daily').textContent = `${data.rain.daily} mm`;
-        document.getElementById('rain.weekly').textContent = `${data.rain.weekly} mm`;
-        document.getElementById('rain.monthly').textContent = `${data.rain.monthly} mm`;
-        document.getElementById('rain.yearly').textContent = `${data.rain.yearly} mm`;
-        document.getElementById('rain.event').textContent = `${data.rain.event} mm`;
-        document.getElementById('rain.rate').textContent = `${data.rain.rate} mm/h`;
-
-        document.getElementById('outdoor.temperature').textContent = `${data.outdoor.temperature} °C`;
-        document.getElementById('outdoor.app_temp').textContent = `${data.outdoor.app_temp} °C`;
-        document.getElementById('outdoor.feels_like').textContent = `${data.outdoor.feels_like} °C`;
-        document.getElementById('outdoor.humidity').textContent = `${data.outdoor.humidity} %`;
-        document.getElementById('outdoor.dew_point').textContent = `${data.outdoor.dew_point} °C`;
-
-        document.getElementById('id').textContent = data.id;
         document.getElementById('timestamp').textContent = data.timestamp;
+        document.getElementById('solar.solar').textContent = `${data.solar_solar} W/m²`;
+        document.getElementById('solar.uvi').textContent = data.solar_uvi;
+
+        document.getElementById('wind.gust').textContent = `${data.wind_gust} km/h`;
+        document.getElementById('wind.direction').textContent = `${data.wind_direction} °`;
+        document.getElementById('wind.speed').textContent = `${data.wind_speed} km/h`;
+
+        document.getElementById('pressure.absolute').textContent = `${data.pressure_absolute} hPa`;
+        document.getElementById('pressure.relative').textContent = `${data.pressure_relative} hPa`;
+
+        document.getElementById('rain.hourly').textContent = `${data.rain_hourly} mm`;
+        document.getElementById('rain.daily').textContent = `${data.rain_daily} mm`;
+        document.getElementById('rain.weekly').textContent = `${data.rain_weekly} mm`;
+        document.getElementById('rain.monthly').textContent = `${data.rain_monthly} mm`;
+        document.getElementById('rain.yearly').textContent = `${data.rain_yearly} mm`;
+        document.getElementById('rain.event').textContent = `${data.rain_event} mm`;
+        document.getElementById('rain.rate').textContent = `${data.rain_rate} mm/h`;
+
+        document.getElementById('outdoor.temperature').textContent = `${data.outdoor_temperature} °C`;
+        document.getElementById('outdoor.app_temp').textContent = `${data.outdoor_app_temp} °C`;
+        document.getElementById('outdoor.feels_like').textContent = `${data.outdoor_feels_like} °C`;
+        document.getElementById('outdoor.humidity').textContent = `${data.outdoor_humidity} %`;
+        document.getElementById('outdoor.dew_point').textContent = `${data.outdoor_dew_point} °C`;
     };
 
     socket.onclose = () => {
